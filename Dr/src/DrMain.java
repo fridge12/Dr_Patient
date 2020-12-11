65a6a0dd4294a893de5958d66eeaf466c342e6fd
fridge12 <ahanwadhwana04@gmail.com>
2020-12-11 22:45:22 +0530

    update 1
    converted array to array list to make it easier to add and remove object
    made methods for creating and destroying objects
    called the garbage collector
    added new login window button (probably will remove)
    added button to admin frame to go the the login window

--- Dr/src/DrMain.java
+++ Dr/src/DrMain.java
@@ -10,6 +10,10 @@ public class DrMain {
 	//making new colours
 	public static final Color DARK_GREEN = new Color (0,200,0);
 	public static final Color DARK_RED = new Color (200,0,0);
+	public static final Color DARK_BLUE = new Color (0,0,200);
+	public static final Color DARK_CYAN = new Color (0,200,200);
+
+
 	//a frame for login
 	JFrame loginFrame;
 	//a frame for the admin
@@ -17,19 +21,72 @@ public class DrMain {
 	// used to keep track of how many frames are open. So I can quit the entire program once the last frame is closed
 	static int frameCounter=0;
 	//creating an array to hold all the objects
-	static DrMain objArray[ ]= new DrMain[10];
-	public static void main ( String args[]) {
-		//DrMain loginObj = new DrMain();
-		objArray[0]= new DrMain();
-		objArray[0].createLoginFrame();
-		objArray[0].createLoginContents(objArray[0].loginFrame,0);
-		
-		objArray[1]= new DrMain();
-		objArray[1].createLoginFrame();
-		objArray[1].createLoginContents(objArray[1].loginFrame,1);
+	/**static DrMain objArray[ ]= new DrMain[10];*/
+	//creating an arraylist to hold all the objects to make it easier to add and remove objects.
+	static ArrayList<DrMain> objArray = new ArrayList<DrMain>();
 	
-			    
+	public static void main ( String args[]) {
+		System.out.println("Free memory before array "+Runtime.getRuntime().freeMemory());
+		
+		
+		DrMain.createObjects();
+		
+		/**objArray.add(new DrMain());
+		objArray.get(0).createLoginFrame();
+		objArray.get(0).createLoginContents(objArray.get(0).loginFrame,0);
+		
+		objArray.add(new DrMain());
+		objArray.get(1).createLoginFrame();
+		objArray.get(1).createLoginContents(objArray.get(1).loginFrame,1);*/
 	}
+	
+	//method for creating an object and put it in objArray
+	public static void createObjects() {
+		//I am setting this variable before I add an object because if I set if after I will have to do -1 from the size
+		int length=objArray.size();
+		
+		//adding an object to the array
+		objArray.add(new DrMain());
+		//creating login frame
+		objArray.get(length).createLoginFrame(length);
+		//creating the contents of that frame
+		objArray.get(length).createLoginContents(objArray.get(length).loginFrame,length);
+		System.out.println("Free memory after array of length"+length+"is "+Runtime.getRuntime().freeMemory());
+		
+	}
+	
+	//method to delete objects and remove then from objArray
+	public static void destoryObjects(int objPosition) {
+		//checking if the adminframe of that object is active or not
+		try {
+		if(objArray.get(objPosition).adminFrame.isActive()) {
+			//making it invisible
+			objArray.get(objPosition).adminFrame.setVisible(false);
+			//deleting adminFrame
+			objArray.get(objPosition).adminFrame.dispose();
+		}
+		}
+		catch(Exception e) {			
+			System.out.println(e);
+			System.out.println("admin frame had not been created");
+		}
+		
+		
+		//checking if the login frame of that object is active or not
+		if(objArray.get(objPosition).loginFrame.isActive()) {
+			//making it invisible
+			objArray.get(objPosition).loginFrame.setVisible(false);
+			//deleting loginFrame
+			objArray.get(objPosition).loginFrame.dispose();
+		}
+		
+		objArray.remove(objPosition);
+		System.out.println("Free memory before gc "+Runtime.getRuntime().freeMemory());
+		System.gc();
+		System.out.println("Free memory after gc "+Runtime.getRuntime().freeMemory());
+		
+	}
+	
 	//method to create contents of the login frame
 	public void createLoginContents(JFrame frame, int objPosition) {
 		//creating the admin button
@@ -37,10 +94,10 @@ public class DrMain {
 		adminButton.setBounds(100, 100, 200, 100);
 		adminButton.setBackground(Color.GREEN);
 		adminButton.setOpaque(true);
-	    adminButton.setBorderPainted(false);	    
+	    adminButton.setBorderPainted(false);	
+	    
 	    //adding a mouse listener to the button so that I can keep the colour changed for the duration of the button being pressed.
-		adminButton.addMouseListener(new MouseAdapter (){
-			
+		adminButton.addMouseListener(new MouseAdapter (){			
 			//changing colour to dark green when the button is selected
 		    public void mousePressed(MouseEvent e) {
 		    	adminButton.setBackground(DARK_GREEN);
@@ -53,7 +110,9 @@ public class DrMain {
 				//printing the object that calls this function
 				System.out.println(this.hashCode()+" "+this.getClass());
 				
-				objArray[objPosition].createAdminFrame();
+				//I am not using frame here because it won't work, that is why I have to use objArray.get
+				objArray.get(objPosition).createAdminFrame(objPosition);
+				objArray.get(objPosition).createAdminContents(objArray.get(objPosition).adminFrame, objPosition);
 				
 			}
 
@@ -71,6 +130,8 @@ public class DrMain {
 		employeeButton.setBackground(Color.RED);
 		employeeButton.setOpaque(true);
 		employeeButton.setBorderPainted(false);
+		
+		//if something isn't commentted go to where I added a mouse listener to the admin button
 		employeeButton.addMouseListener(new MouseAdapter(){
 			public void mousePressed(MouseEvent e) {
 				employeeButton.setBackground(DARK_RED);
@@ -88,13 +149,41 @@ public class DrMain {
 		//adding employeebutton the frame of the object
 		frame.add(employeeButton);
 		
+		//creating button to create a new login frame
+		JButton newLoginFrameButton = new JButton ("New login window");
+		newLoginFrameButton.setBounds(500,100,200,100);
+		newLoginFrameButton.setBackground(Color.CYAN);
+		newLoginFrameButton.setOpaque(true);
+		newLoginFrameButton.setBorderPainted(false);
+		
+		//if something isn't commentted go to where I added a mouse listener to the admin button
+		newLoginFrameButton.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e) {
+				newLoginFrameButton.setBackground(DARK_CYAN);
+				
+			}
+			
+			public void mouseReleased(MouseEvent e) {
+				newLoginFrameButton.setBackground(Color.CYAN);
+				Toolkit.getDefaultToolkit().beep();
+				//calling the function which creates new objects
+				DrMain.createObjects();
+				
+			}
+			
+		});
+		
+		//adding employeebutton the frame of the object
+		frame.add(newLoginFrameButton);
 		
 		
 		
 	}
 	
+	
+	
 	//method to create the login frame
-	public void createLoginFrame() {
+	public void createLoginFrame(int objPosition) {
 		this.loginFrame= new JFrame("Login Window");
 		//adding 1 to frame counter 
 		frameCounter++;
@@ -116,6 +205,8 @@ public class DrMain {
 	          }
 	          else {
 	        	 frameCounter--;
+	        	 //calling a function to delete the object
+	        	 DrMain.destoryObjects(objPosition);
 	        	 System.out.println(frameCounter);
 	          }
 	        
@@ -124,8 +215,14 @@ public class DrMain {
 	}
 	
 	
+	
 	//method to create admin frame
-	public void createAdminFrame() {
+	public void createAdminFrame(int objPosition) {
+		//making the previous frame invisible
+		objArray.get(objPosition).loginFrame.setVisible(false);
+		//decreasing frame counter by 1 because the login frame is now invisible
+		frameCounter--;
+		
 		this.adminFrame = new JFrame("Admin Frame");
 		//adding 1 to frame counter
 		frameCounter++;
@@ -136,6 +233,52 @@ public class DrMain {
 		this.adminFrame.setLocation(100,100);
 		this.adminFrame.setLayout(null); 		
 		this.adminFrame.setVisible(true);
+		this.adminFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+	        @Override
+	        public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+	        // if the last window is closed then the whole program terminates, else framecounter is decreased by 1	
+	          if(frameCounter==1) {
+	        	  System.out.println(frameCounter);
+	        	  System.exit(0);		          
+	          }
+	          else {
+	        	 frameCounter--;
+	        	 System.out.println(frameCounter);
+	          }
+	        
+	        }
+	    });
+	}
+	
+	
+	
+	public void createAdminContents(JFrame frame, int objPosition) {
+		//creating button to go to the previous frame
+		JButton previousFrameButton = new JButton("Go to login window");
+		previousFrameButton.setBounds(00,0,200,100);
+		previousFrameButton.setBackground(Color.RED);
+		previousFrameButton.setOpaque(true);
+		previousFrameButton.setBorderPainted(false);
+		
+		previousFrameButton.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e) {
+				previousFrameButton.setBackground(DARK_RED);
+				
+			}
+			
+			public void mouseReleased(MouseEvent e) {
+				previousFrameButton.setBackground(Color.RED);
+				Toolkit.getDefaultToolkit().beep();
+				objArray.get(objPosition).loginFrame.setVisible(true);
+				frame.dispose();
+
+			}
+			
+		});
+		
+		frame.add(previousFrameButton);
+		
+		System.out.println("admin frame contents created");
 	}
 	
 
